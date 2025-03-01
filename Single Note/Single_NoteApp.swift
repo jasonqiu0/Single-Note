974c1dc60dd2982da25088eac30f79f829add361
jasonqiu0 <jasqiu2@gmail.com>
2025-03-01 19:14:31 +0700

    Initial Commit

---
+++ Single Note/Single_NoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Single_NoteApp.swift
+//  Single Note
+//
+//  Created by Jason Qiu on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Single_NoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Single NoteTests/Single_NoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  Single_NoteTests.swift
+//  Single NoteTests
+//
+//  Created by Jason Qiu on 3/1/25.
+//
+
+import Testing
+@testable import Single_Note
+
+struct Single_NoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
