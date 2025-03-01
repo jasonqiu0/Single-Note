01a5cde45fe6c8e69520c54421837f16eb25e682
Jason Qiu <jasqiu2@gmail.com>
2025-03-01 20:18:23 +0700

    Add files via upload

---
+++ Single Note/View/Home.swift
@@ -0,0 +1,23 @@
+//
+//  Home.swift
+//  Single Note
+//
+//  Created by Jason Qiu on 3/1/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .frame(width: 300, height: 100)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
